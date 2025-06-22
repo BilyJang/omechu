45b3b3ff6d5dc12a1f1f3cec19045945b3d84117
BilyJang <as9602@naver.com>
2025-06-22 23:53:35 +0900

    Create _app.tsx

---
+++ pages/_app.tsx
@@ -0,0 +1,20 @@
+import type { AppProps } from 'next/app'
+import '../styles/globals.css'
+import Head from 'next/head'
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.NEXT_PUBLIC_KAKAO_REST_API_KEY = '${process.env.NEXT_PUBLIC_KAKAO_REST_API_KEY || ''}';
+            `
+          }}
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
+}
